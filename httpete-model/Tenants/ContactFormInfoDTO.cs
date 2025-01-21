963e0b7599308aa55a91f8376fc55186d24f2f53
silvahimself <david@silvahimself.com>
2025-01-21 19:50:57 +0000

    Add ContactFormInfoDTO

---
+++ httpete-model/Tenants/ContactFormInfoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HttPete.Model.Tenants
+{
+    public class ContactFormInfoDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
